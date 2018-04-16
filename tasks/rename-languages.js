69eae565965525d0fe4f63f8660801fdf184ab59
David Ayala <davixyz@gmail.com>
2018-04-16 16:22:10 +0200

    rename files nodejs

--- tasks/rename-languages.js
+++ tasks/rename-languages.js
@@ -1,53 +1,51 @@
 var fs = require('fs');
 var path = require('path');
-var hugoLanguages = ["ca", "es", "en"];
+var siteLanguages = ["ca", "es", "en"];
+
+function renameFile(_old, _new){
+  fs.rename(_old, _new, function(err) {
+      if(err){
+        console.log('Error: ' + err);
+      }
+  });
+}
 
 var walk = function(dir, done) {
-  var results = [];
-
   fs.readdir(dir, function(err, list) {
     if (err) return done(err);
 
     var pending = list.length;
-    if (!pending) return done(null, results);
+
+    if (!pending) return done(null, "ok");
 
     list.forEach(function(file) {
       file = path.resolve(dir, file);
       fs.stat(file, function(err, stat) {
         if (stat && stat.isDirectory()) {
           walk(file, function(err, res) {
-            results = results.concat(res);
-            if (!--pending) done(null, results);
+            if (!--pending) done(null, "ok");
           });
         } else {
-          results.push(file);
-          if (!--pending) done(null, results);
+          //rename file
+          var pos = -1;
+          for(var l=0, ls=siteLanguages.length; l<ls; l++){
+            pos = file.indexOf("-"+siteLanguages[l]+".md");
+            if(pos>-1){
+              renameFile(file, file.slice(0,pos)+"."+siteLanguages[l]+".md");
+            }
+          }
+          if (!--pending) done(null, "ok");
         }
       });
     });
 
   });
-
 };
 
 //Renames netlifycms language files with "-[lang].md" to ".[lang].md"
-walk("./content", function(err, results){
+walk("./content", function(err, message){
   if(err){
     console.log("error " + err);
     return;
-  }
-
-  var pos;
-  for(var i=0, z=results.length; i<z; i++){
-    for(var l=0, ls=hugoLanguages.length; l<ls; l++){
-      pos = results[i].indexOf("-"+hugoLanguages[l]+".md");
-      if(pos>-1){
-        fs.rename(results[i], results[i].slice(0,pos)+"."+hugoLanguages[l]+".md", function(err) {
-            if(err){
-              console.log('Error: ' + err);
-            }
-        });
-      }
-    }
   }
 })
